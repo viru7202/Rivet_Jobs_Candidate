4c52d31f1fe4f6dabe0cff7602d14113c20f7d76
viru7202 <viren@torbitmultisoft.com>
2024-11-27 17:04:13 +0530

    changes

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary navbarweb">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/webdeshbord">
+          <Link className="navbar-brand" to="/">
             <img src="assets/img/rivetcare_logo.png" alt="" />
           </Link>
           {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
